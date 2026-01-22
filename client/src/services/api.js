00416773ea4fc93a172b70e4829a326a1fded910
swagduck <vtuy2004@gmail.com>
2026-01-22 14:25:00 +0700

    Hardcode API URL to fix connection issue immediately

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api" });
+const API = axios.create({ baseURL: "https://wedding-f35z.onrender.com/api" });
 
 export const fetchPhotos = () => API.get("/photos");
 export const uploadPhoto = (formData) => API.post("/upload", formData);
